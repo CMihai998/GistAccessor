1d20ceef2686f968c31e6587a3f1a5a39c36ed9b
Mihai Circu <mihcircu@amazon.com>
2021-11-06 19:36:18 +0100

    Added following functionalities: - now the latest users who forked the project will be displayed under "Forked by" column - now each file will be displayed with it's tag/badge under the "File type" column

--- src/app/gist-components/search-gists/search-gists.component.ts
+++ src/app/gist-components/search-gists/search-gists.component.ts
@@ -14,8 +14,52 @@ export class SearchGistsComponent implements OnInit {
   }
 
   searchGists(username: String) {
-    this.octokit.request(`GET /users/${username}/gists`)
-      .then(response => this.gists = response.data)
+    this.octokit.request(`GET /users/${username}/gists`, {
+      headers: {
+        authorization: "token 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"
+      }
+    }).then(response => {
+        this.gists = response.data
+        for (const gist of this.gists) {
+          this.setForksAndFileType(gist)
+        }
+      })
+  }
+
+  setForksAndFileType(gist: any) {
+    this.setForks(gist)
+    this.setFileData(gist)
+  }
+
+  setForks(gist: any) {
+      this.octokit.request(`GET /gists/${gist.id}/forks`, {
+        headers: {
+          authorization: "token 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"
+        }
+      }).then(response => {
+        gist.forks_list = []
+
+        if (response.data.length > 0) {
+          response.data.sort((a: any, b: any) =>
+            (Date.parse(a.created_at).valueOf() > Date.parse(b.created_at).valueOf())
+          ? 1 : -1)
+
+          for (let i = 0; i < 3; i++) {
+            if (i >= response.data.length)
+              break
+            gist.forks_list.push({"login": response.data[i].owner.login,
+                                  "avatar_url": response.data[i].owner.avatar_url})
+          }
+        }
+        })
+  }
+
+  setFileData(gist: any) {
+    gist.file_types = []
+    for (const [fileName, fileData] of Object.entries(gist.files)) {
+        // @ts-ignore
+      gist.file_types.push(`${fileName}: ${fileData.type}`)
+    }
   }
 
 }
